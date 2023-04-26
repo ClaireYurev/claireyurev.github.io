ba6a9561862c056b6136f19e467368115d02ac15
yamidduarte <yamid9@gmail.com>
2023-04-26 14:15:43 -0700

    Software Developer

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ export default function HomePage() {
         <div className={styles.foreground}>
           <div className={styles.content}>
             <h1 className={styles.name}>Claire Yurev</h1>
-            <h6 className={styles.bio}>Aspiring Developer</h6>
+            <h6 className={styles.bio}>Software Developer</h6>
             <Link href="/projects">
               <button className={styles.button}>View Projects</button>
             </Link>
